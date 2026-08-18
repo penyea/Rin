e493b90dc0fab131fd9293de752ebe2f2ae7e723
Preston <130431768+penyea@users.noreply.github.com>
2026-08-18 16:52:36 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,118 +1,26 @@
-import { useEffect } from "react";
-
-import { AppProviders } from "./app/providers";
-import { AppRoutes } from "./app/routes";
-import { useAppBootstrap } from "./app/use-app-bootstrap";
-
-
-function Adcash(){
-
-  useEffect(()=>{
-
-
-    if(document.getElementById("aclib-script")){
-      return;
-    }
-
-
-    const script = document.createElement("script");
-
-
-    script.id = "aclib-script";
-
-
-    script.src =
-      "https://acscdn.com/script/aclib.js";
-
-
-    script.async = true;
-
-
-    script.onload = ()=>{
-
-
-      try{
-
-
-        if(
-          window.aclib &&
-          typeof window.aclib.runAutoTag === "function"
-        ){
-
-
-          window.aclib.runAutoTag({
-
-            zoneId:"ydqubl9osj"
-
-          });
-
-
-          console.log(
-            "Adcash initialized"
-          );
-
-
-        }
-
-
-      }catch(error){
-
-
-        console.log(
-          "Adcash error:",
-          error
-        );
-
-
-      }
-
-
-    };
-
-
-    document.head.appendChild(script);
-
-
-
-  },[]);
-
-
-
-  return null;
-
-}
-
+import Adcash from "./components/Adcash";
 
 
 function App() {
 
-
-  const { config, profile } = useAppBootstrap();
-
+const { config, profile } = useAppBootstrap();
 
 
-  return (
+return (
 
-    <>
+<>
 
-      <Adcash />
+<Adcash />
 
+<AppProviders config={config} profile={profile}>
 
-      <AppProviders 
-        config={config} 
-        profile={profile}
-      >
+<AppRoutes />
 
-        <AppRoutes />
-
-      </AppProviders>
+</AppProviders>
 
 
-    </>
+</>
 
-  )
+)
 
 }
-
-
-export default App;
