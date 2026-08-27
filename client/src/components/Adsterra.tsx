7fffa9782df11957b706b4856814abe7cf8cadc8
Preston <130431768+penyea@users.noreply.github.com>
2026-08-27 23:29:05 +0800

    Update Adsterra.tsx

--- client/src/components/Adsterra.tsx
+++ client/src/components/Adsterra.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Script from "next/script";
 
 export default function Adsterra() {
@@ -9,7 +7,6 @@ export default function Adsterra() {
       {/* Popunder */}
       <Script
         src="https://lightlyenergeticevolution.com/42/e4/b9/42e4b960a9019cd24b6f7d704220d5e0.js"
-        strategy="afterInteractive"
       />
 
 
@@ -18,63 +15,48 @@ export default function Adsterra() {
         async
         data-cfasync="false"
         src="https://lightlyenergeticevolution.com/4e4fb4b34886bd0b9ccefd8663bd8cf5/invoke.js"
-        strategy="afterInteractive"
       />
 
-      <div id="container-4e4fb4b34886bd0b9ccefd8663bd8cf5"></div>
+      <div id="container-4e4fb4b34886bd0b9ccefd8663bd8cf5" />
 
 
       {/* Smartlink */}
-      <a
-        href="https://lightlyenergeticevolution.com/ywa9xt5j5?key=13ceed1f3955e0f23cf447a0e0e64f5f"
-        target="_blank"
-      >
+      <a href="https://lightlyenergeticevolution.com/ywa9xt5j5?key=13ceed1f3955e0f23cf447a0e0e64f5f">
         Smartlink
       </a>
 
 
-      {/* 468x60 */}
-      <AdBlock
+      <Banner
         keyId="262b4c103fcd51fa540fe9de0a149940"
         width={468}
         height={60}
       />
 
-
-      {/* 300x250 */}
-      <AdBlock
+      <Banner
         keyId="76141def117058a2ee6c4eb90b211812"
         width={300}
         height={250}
       />
 
-
-      {/* 160x600 */}
-      <AdBlock
+      <Banner
         keyId="cff874454a9cb0638cfd626004c56b85"
         width={160}
         height={600}
       />
 
-
-      {/* 160x300 */}
-      <AdBlock
+      <Banner
         keyId="82a7c7c3d11cded985ea1adaa105f226"
         width={160}
         height={300}
       />
 
-
-      {/* 320x50 */}
-      <AdBlock
+      <Banner
         keyId="c2fbff91e24d85454c1fc5fd739bcd86"
         width={320}
         height={50}
       />
 
-
-      {/* 728x90 */}
-      <AdBlock
+      <Banner
         keyId="9a8f7e7cda176cfd31394e27113dea5b"
         width={728}
         height={90}
@@ -86,29 +68,25 @@ export default function Adsterra() {
 }
 
 
-
-function AdBlock({
-  keyId,
-  width,
-  height,
-}: {
-  keyId:string;
-  width:number;
-  height:number;
-}) {
+function Banner({
+ keyId,
+ width,
+ height
+}:{
+ keyId:string;
+ width:number;
+ height:number;
+}){
 
 return (
 
 <div style={{
-  margin:"20px auto",
-  textAlign:"center"
+ margin:"20px auto",
+ textAlign:"center"
 }}>
 
-
-<Script
-id={keyId}
-strategy="afterInteractive"
-dangerouslySetInnerHTML={{
+<script
+ dangerouslySetInnerHTML={{
 __html:`
 
 atOptions = {
@@ -124,9 +102,8 @@ atOptions = {
 />
 
 
-<Script
-strategy="afterInteractive"
-src={`https://lightlyenergeticevolution.com/${keyId}/invoke.js`}
+<script
+ src={`https://lightlyenergeticevolution.com/${keyId}/invoke.js`}
 />
 
 
