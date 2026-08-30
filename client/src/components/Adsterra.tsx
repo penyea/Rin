5518cb5672dfbc930cd960a53a50edf05ec0e3c9
Preston <130431768+penyea@users.noreply.github.com>
2026-08-30 18:19:15 +0800

    Update Adsterra.tsx

--- client/src/components/Adsterra.tsx
+++ client/src/components/Adsterra.tsx
@@ -1,84 +1,47 @@
 import { useEffect } from "react";
 
 export default function Adsterra() {
-
   useEffect(() => {
+    const script = document.createElement("script");
 
-    const loadAd = () => {
+    script.innerHTML = `
+      atOptions = {
+        'key' : '9a8f7e7cda176cfd31394e27113dea5b',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    `;
 
-      const script = document.createElement("script");
-
-      script.innerHTML = `
-        atOptions = {
-          'key' : '9a8f7e7cda176cfd31394e27113dea5b',
-          'format' : 'iframe',
-          'height' : 90,
-          'width' : 728,
-          'params' : {}
-        };
-      `;
-
-      document.body.appendChild(script);
+    document.body.appendChild(script);
 
 
-      const invoke = document.createElement("script");
+    const invoke = document.createElement("script");
 
-      invoke.src =
+    invoke.src =
       "https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
 
-      invoke.async = true;
+    invoke.async = true;
 
-      document.body.appendChild(invoke);
+    document.body.appendChild(invoke);
 
+
+    return () => {
+      document.body.removeChild(script);
+      document.body.removeChild(invoke);
     };
-
-
-    loadAd();
-
 
   }, []);
 
 
   return (
-
-    <>
-
-      {/* 左侧广告 */}
-
-      <div
-        id="adsterra-left"
-        style={{
-          position:"fixed",
-          left:"0",
-          top:"120px",
-          width:"728px",
-          height:"90px",
-          zIndex:99999,
-          transform:"rotate(-90deg)",
-          transformOrigin:"left top"
-        }}
-      />
-
-
-      {/* 右侧广告 */}
-
-      <div
-        id="adsterra-right"
-        style={{
-          position:"fixed",
-          right:"0",
-          top:"120px",
-          width:"728px",
-          height:"90px",
-          zIndex:99999,
-          transform:"rotate(90deg)",
-          transformOrigin:"right top"
-        }}
-      />
-
-
-    </>
-
+    <div
+      style={{
+        width:"728px",
+        height:"90px",
+        margin:"20px auto",
+      }}
+    />
   );
-
 }
