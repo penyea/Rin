578097f2ae9f752bb660673157b96de36d1c2064
Preston <130431768+penyea@users.noreply.github.com>
2026-08-30 21:09:09 +0800

    Update AdSidebar.tsx

--- client/src/components/AdSidebar.tsx
+++ client/src/components/AdSidebar.tsx
@@ -1,33 +1,33 @@
 import { useEffect, useRef } from "react";
 
 export default function AdSidebar() {
+
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+
     if (!ref.current) return;
 
-    const script = document.createElement("script");
+    const s = document.createElement("script");
 
-    script.src =
+    s.src =
       "https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
 
-    script.async = true;
+    s.async = true;
 
-    ref.current.appendChild(script);
+    ref.current.appendChild(s);
 
-    return () => {
-      if (ref.current) {
-        ref.current.innerHTML = "";
-      }
-    };
+
   }, []);
+
 
   return (
     <div
       ref={ref}
       style={{
-        width: "120px",
-        height: "600px",
+        width:"120px",
+        height:"600px",
+        overflow:"hidden"
       }}
     />
   );
