dded9e257215e4b83caa9affd40ef0f572ac7d5b
Preston <130431768+penyea@users.noreply.github.com>
2026-08-30 17:36:20 +0800

    Update Adsterra.tsx

--- client/src/components/Adsterra.tsx
+++ client/src/components/Adsterra.tsx
@@ -2,45 +2,83 @@ import { useEffect } from "react";
 
 export default function Adsterra() {
 
-useEffect(() => {
+  useEffect(() => {
 
-  const script = document.createElement("script");
+    const loadAd = () => {
 
-  script.innerHTML = `
-  atOptions = {
-    'key' : '9a8f7e7cda176cfd31394e27113dea5b',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
-  };
-  `;
+      const script = document.createElement("script");
 
-  document.body.appendChild(script);
+      script.innerHTML = `
+        atOptions = {
+          'key' : '9a8f7e7cda176cfd31394e27113dea5b',
+          'format' : 'iframe',
+          'height' : 90,
+          'width' : 728,
+          'params' : {}
+        };
+      `;
 
-
-  const invoke = document.createElement("script");
-
-  invoke.src =
-  "https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
-
-  invoke.async = true;
-
-  document.body.appendChild(invoke);
+      document.body.appendChild(script);
 
 
-},[]);
+      const invoke = document.createElement("script");
+
+      invoke.src =
+      "https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
+
+      invoke.async = true;
+
+      document.body.appendChild(invoke);
+
+    };
 
 
-return (
-  <div
-    id="adsterra-banner"
-    style={{
-      width:"728px",
-      height:"90px",
-      margin:"20px auto"
-    }}
-  />
-)
+    loadAd();
+
+
+  }, []);
+
+
+  return (
+
+    <>
+
+      {/* 左侧广告 */}
+
+      <div
+        id="adsterra-left"
+        style={{
+          position:"fixed",
+          left:"0",
+          top:"120px",
+          width:"728px",
+          height:"90px",
+          zIndex:99999,
+          transform:"rotate(-90deg)",
+          transformOrigin:"left top"
+        }}
+      />
+
+
+      {/* 右侧广告 */}
+
+      <div
+        id="adsterra-right"
+        style={{
+          position:"fixed",
+          right:"0",
+          top:"120px",
+          width:"728px",
+          height:"90px",
+          zIndex:99999,
+          transform:"rotate(90deg)",
+          transformOrigin:"right top"
+        }}
+      />
+
+
+    </>
+
+  );
 
 }
