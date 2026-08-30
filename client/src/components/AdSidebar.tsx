eb556adaea391a8e208f97f0d12f607c26fa7a0c
Preston <130431768+penyea@users.noreply.github.com>
2026-08-30 18:39:09 +0800

    Update AdSidebar.tsx

--- client/src/components/AdSidebar.tsx
+++ client/src/components/AdSidebar.tsx
@@ -2,32 +2,40 @@ import { useEffect } from "react";
 
 export default function AdSidebar(){
 
-useEffect(()=>{
+  useEffect(()=>{
 
-const s=document.createElement("script");
+    const container = document.getElementById("adsterra-sidebar");
 
-s.src=
-"https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
-
-s.async=true;
-
-document.body.appendChild(s);
+    if (!container) return;
 
 
-return ()=>{
- document.body.removeChild(s);
-}
+    const s = document.createElement("script");
 
-},[]);
+    s.src =
+      "https://lightlyenergeticevolution.com/9a8f7e7cda176cfd31394e27113dea5b/invoke.js";
+
+    s.async = true;
 
 
-return (
-<div
-style={{
-width:"160px",
-height:"600px"
-}}
-/>
-)
+    container.appendChild(s);
+
+
+    return ()=>{
+      container.removeChild(s);
+    };
+
+
+  },[]);
+
+
+  return (
+    <div
+      id="adsterra-sidebar"
+      style={{
+        width:"160px",
+        height:"600px",
+      }}
+    />
+  );
 
 }
